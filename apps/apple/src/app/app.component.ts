95ee8bd211840a1ed3cfcc1af1ec0b399f7e01ae
Jim Drury <jim@jimdrury.co.uk>
2022-06-14 00:33:08 +0100

    adds apple airpod example

--- apps/apple/src/app/app.component.ts
+++ apps/apple/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {NgtGLTFLoader} from '@angular-three/soba/loaders';
+import type {GLTF} from 'three-stdlib/loaders/GLTFLoader';
+import type {NgtObjectMap, NgtTriple} from '@angular-three/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'iconic-homepages-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'apple';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('container', {static: true})
+  container!: ElementRef<HTMLDivElement>;
+
+  animation?: GLTF & NgtObjectMap;
+  lightPosition = {
+    x: -3,
+    y: 3,
+    z: 7,
+  };
+
+  get lightPositionTriple(): NgtTriple {
+    const {x, y, z} = this.lightPosition;
+    return [x, y, z];
+  }
+
+  constructor(
+    private loader: NgtGLTFLoader) {
+  }
+
+  ngOnInit() {
+    this.loader
+      .load('assets/assunta-valentino_airpods-liv-1.gltf')
+      .subscribe((gltf: any) => {
+        const node = gltf.nodes['Oggetto_1'];
+        node.material.metalness = 0.9;
+        node.material.roughness = 0;
+        this.animation = gltf;
+      });
+  }
+
+  ngAfterViewInit() {
+    const st = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.container.nativeElement,
+        start: 'top top',
+        end: 'bottom center',
+        scrub: 1,
+        snap: {
+          snapTo: 'labels',
+          duration: {min: 0.2, max: 3},
+          delay: 0.2,
+          ease: 'power1.inOut',
+        },
+      },
+    });
+
+    st.to(this.lightPosition, {x: 0, y: 0, z: -1});
+
+  }
+
 }
--- apps/apple/src/app/app.module.ts
+++ apps/apple/src/app/app.module.ts
@@ -1,13 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgtCanvasModule} from '@angular-three/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  NgtPrimitiveModule,
+} from '@angular-three/core/primitive';
+
+import {
+  NgtAmbientLightModule,
+  NgtSpotLightModule,
+  NgtPointLightModule,
+} from '@angular-three/core/lights';
+
+import {
+  NgtSobaOrbitControlsModule,
+} from '@angular-three/soba/controls';
+
+import {
+  NgtPointLightHelperModule,
+  NgtSpotLightHelperModule
+} from '@angular-three/core/helpers';
+
+
+import {AppComponent} from './app.component';
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NgtCanvasModule,
+    NgtCanvasModule,
+    NgtAmbientLightModule,
+    NgtSpotLightModule,
+    NgtPointLightModule,
+    NgtSobaOrbitControlsModule,
+    NgtPrimitiveModule,
+    NgtSpotLightHelperModule,
+    NgtPointLightHelperModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
